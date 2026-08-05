1de06704f68d74d545b3981e9e8eb9547f816688
dclarktech302 <dcps302@gmail.com>
2026-08-05 00:50:18 -0400

    added some logging

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "bucket_name" {
   type = string
+  validation {
+    condition     = can(regex("^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$", var.bucket_name))
+    error_message = "bucket_name must be 3-63 chars, lowercase letters/numbers/hyphens/periods only, and start/end with a letter or number."
+  }
 }

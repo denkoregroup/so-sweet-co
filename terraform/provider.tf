6d63f91b1de3e4ca5904b6d4d730b4698257553c
dclarktech302 <dcps302@gmail.com>
2026-08-04 23:28:58 -0400

    set up s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,13 @@
-provider
+provider "aws" {
+  
+}
+
+terraform {
+    backend "s3" {
+        bucket = "tf-resources-github-actions"
+        region = "us-east-1"
+        key = "github-actions/terraform.tfstate"
+        encrypt = true
+        dynamodb_table = "tf-resources-github-actions"
+    }
+}
